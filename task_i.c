21ab821b00d4e9e7e747e007304d81ce64a9667c
wuxianye <164884853+wuxianye@users.noreply.github.com>
2024-05-04 16:28:43 +0100

    Add files via upload

--- task_i.c
+++ task_i.c
@@ -1,88 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h> // Used for fabs function
+#include <math.h>
 
-// Exchange function
-void swap(double* a, double* b) {
-	double t = *a;
-	*a = *b;
-	*b = t;
-}
-// Quick sort function for sorting
-void quickSort(double arr[], int low, int high) {
-	if (low < high) {
-		int pi = partition(arr, low, high);
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
-	}
+int compare(const void* a, const void* b) {
+    double f = *(double*)a - *(double*)b;
+    if (f > 0) return 1;
+    if (f < 0) return -1;
+    return 0;
 }
 
-// partition function
-int partition(double arr[], int low, int high) {
-	double pivot = arr[high];
-	int i = (low - 1);
-	for (int j = low; j <= high - 1; j++) {
-		if (arr[j] < pivot) {
-			i++;
-			swap(&arr[i], &arr[j]);
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
-}
-
-
-// The function for calculating the median
 double findMedian(double arr[], int n) {
-	quickSort(arr, 0, n - 1);
-	if (n % 2 != 0)
-		return (double)arr[n / 2];
-	return (double)(arr[(n - 1) / 2] + arr[n / 2]) / 2.0;
+    qsort(arr, n, sizeof(double), compare);
+    if (n % 2 != 0)
+        return (double)arr[n / 2];
+
+    return (double)(arr[(n - 1) / 2] + arr[n / 2]) / 2.0;
 }
 
 int main(void) {
 #define N 201
 #define M 3
-	double data[N][M];
+    double data[N][M];
 
 #define MAXCHAR 100
-	char buffer[MAXCHAR];
-	int row = 0;
-	int column = 0;
-	char* ptr;
+    char buffer[MAXCHAR];
+    int row = 0;
+    int column = 0;
+    char* ptr;
 
-	char fn[] = "data.csv";
-	FILE* fptr;
-	fptr = fopen(fn, "r");
-	if (fptr == NULL) {
-		printf("Error opening file.\n");
-		return EXIT_FAILURE;
-	}
-	while (fgets(buffer, MAXCHAR, fptr)) {
-		column = 0;
-		row++;
-		//if (row == 1) {
-		//	continue; // Skip Title Rows
-		//}
-		char* value = strtok(buffer, ", ");
-		while (value) {
-			data[row - 2][column] = strtod(value, &ptr);
-			value = strtok(NULL, ", ");
-			column++;
-		}
-	}
-	fclose(fptr);
-	fptr = NULL;
+    char fn[] = "data.csv";
+    FILE* fptr;
+    fptr = fopen(fn, "r");
+    if (fptr == NULL) {
+        printf("Error opening file.\n");
+        return EXIT_FAILURE;
+    }
+    while (fgets(buffer, MAXCHAR, fptr)) {
+        column = 0;
+        row++;
+        if (row == 1) {
+            continue;
+        }
+        char* value = strtok(buffer, ", ");
+        while (value) {
+            data[row - 2][column] = strtod(value, &ptr);
+            value = strtok(NULL, ", ");
+            column++;
+        }
+    }
+    fclose(fptr);
+    fptr = NULL;
+    //===========================================================
+    double medians[M];
+    for (int i = 0; i < M; i++) {
+        medians[i] = findMedian(data[i], N);
+    }
 
-	// Calculate the median of each column
-	double medians[M];
-	for (int i = 0; i < M; i++) {
-		medians[i] = findMedian(data[i], N);
-	}
+    printf("%.2f,%.2f,%.2f", medians[0], medians[1], medians[2]);
 
-	// Format output result
-	printf("%.2f,%.2f,%.2f\n", medians[0], medians[1], medians[2]);
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
