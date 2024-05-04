34cd9b3687a6cf3346c7074873d47fd795a1cb70
wuxianye <164884853+wuxianye@users.noreply.github.com>
2024-05-04 17:57:49 +0100

    Add files via upload

--- task_i.c
+++ task_i.c
@@ -1,41 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-
-int compare(const void* a, const void* b) {
-    double f = *(double*)a - *(double*)b;
-    if (f > 0) return 1;
-    if (f < 0) return -1;
-    return 0;
-}
-
-double findMedian(double arr[], int n) {
-    qsort(arr, n, sizeof(double), compare);
-    if (n % 2 != 0)
-        return (double)arr[n / 2];
-
-    return (double)(arr[(n - 1) / 2] + arr[n / 2]) / 2.0;
-}
 
 int main(void) {
-#define N 201
-#define M 3
+
+
+    #define N 201
+    #define M 3
     double data[N][M];
 
-#define MAXCHAR 100
+    #define MAXCHAR 100
     char buffer[MAXCHAR];
     int row = 0;
     int column = 0;
-    char* ptr;
+    char *ptr;
 
     char fn[] = "data.csv";
-    FILE* fptr;
+    FILE *fptr;
     fptr = fopen(fn, "r");
-    if (fptr == NULL) {
-        printf("Error opening file.\n");
-        return EXIT_FAILURE;
-    }
     while (fgets(buffer, MAXCHAR, fptr)) {
         column = 0;
         row++;
@@ -43,21 +25,19 @@ int main(void) {
             continue;
         }
         char* value = strtok(buffer, ", ");
-        while (value) {
-            data[row - 2][column] = strtod(value, &ptr);
+        while (value) {  
+            data[row-2][column] = strtod(value,&ptr);
             value = strtok(NULL, ", ");
             column++;
         }
     }
     fclose(fptr);
     fptr = NULL;
-    //===========================================================
-    double medians[M];
-    for (int i = 0; i < M; i++) {
-        medians[i] = findMedian(data[i], N);
-    }
 
-    printf("%.2f,%.2f,%.2f", medians[0], medians[1], medians[2]);
+
+
+
+
 
     return EXIT_SUCCESS;
 }
